a1e8b35b483308992b273f0dc6d2acdb094261d7
joaogkt <jgabriel.torres@hotmail.com>
2025-03-19 15:57:13 -0300

    adicionado link entre o details team para o details player do jogador clicado. Controle para nao inserir placar negativo e correção do campo de data de nascimento do jogador

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="API GameVision",
         default_version='v1',
-        description="API para obter informações sobre jogadores e suas estatísticas de desempenho.",
+        description="API para obter informações sobre os dados cadastrados no sistema GameVision",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="game.vision.udf@gmail.com"),
     ),
